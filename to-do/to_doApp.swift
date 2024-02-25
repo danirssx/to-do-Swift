5d7a1933be185fb87382207c4db9b578feb0afb1
danirssx <rossdaniel5018@gmail.com>
2024-02-24 16:50:59 -0800

    Initial Commit

---
+++ to-do/to_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_doApp.swift
+//  to-do
+//
+//  Created by Daniel Ross on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct to_doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
