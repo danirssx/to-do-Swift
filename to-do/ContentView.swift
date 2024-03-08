4f0dffee30645b155413163cf9bef47a1c600e06
danirssx <rossdaniel5018@gmail.com>
2024-03-08 07:36:53 -0800

    Added modals to the Menu

--- to-do/ContentView.swift
+++ to-do/ContentView.swift
@@ -17,11 +17,53 @@ struct ContentView: View {
     var body: some View {
         
         NavigationView {
-//                TaskView(todos: $todos)
-//                    .padding()
-                ListView(todos: $todos, selectedItem: $selectedItem)
+            ScrollView {
+                    VStack {
+                        
+                        Section {
+                            NavigationLink(destination: ListView(todos: $todos, selectedItem: $selectedItem)) {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .frame(width: 350, height: 175)
+                                        .overlay(
+                                            LinearGradient(gradient: .init(colors: [.yellow, .orange, .red]),
+                                                           startPoint: .topLeading,
+                                                           endPoint: .bottomTrailing))
+                                        .cornerRadius(20)
+                                    
+                                    ZStack {
+                                        Text("Tasks")
+                                            .font(.system(size: 40, weight: .bold, design: .rounded))
+                                            .foregroundColor(.white)
+                                            .offset(x: -60, y: -40)
+                                            .multilineTextAlignment(.leading)
+                                        
+                                        Rectangle()
+                                            .frame(height: 2)
+                                            .offset(y: -10)
+                                            .foregroundColor(Color(.systemGray5).opacity(0.3))
+                                        
+                                        Text("Here you can see your tasks and the one of your friends.")
+                                            .foregroundColor(.white)
+                                            .multilineTextAlignment(.leading)
+                                            .bold()
+                                            .offset(y: 20)
+                                    }.padding(35)
+                                    
+                                    
+                                }.frame(width: 350, height: 170)
+                            }
+                        }.padding([.bottom, .top], 20)
+                        
+//                        Another Text
+                        
+                            Text("daniel")
+                                .foregroundColor(.blue)
+
+                }
+                .navigationTitle("Menu")
+            }
             
-            .navigationTitle("To-Do App")
         }
     }
 }
